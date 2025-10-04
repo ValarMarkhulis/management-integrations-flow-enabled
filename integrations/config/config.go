38042702b4f2f227fc86a13dbdd0c55fceb0353e
Christian Alexander Sauer Mark <11947367+ValarMarkhulis@users.noreply.github.com>
2025-10-04 17:33:12 +0200

    Fix typo in NB_FlowIntervalInMinuets

--- integrations/config/config.go
+++ integrations/config/config.go
@@ -33,18 +33,18 @@ func ExtendNetBirdConfig(peerID string, config *proto.NetbirdConfig, extraSettin
 		log.Debugf("Env 'NB_FLOW_URL' was set to: '%s'", FlowURL)
 	}
 
-	NB_FlowIntervalInMinuets := os.Getenv("NB_FlowIntervalInMinuets")
-	if NB_FlowIntervalInMinuets == "" {
+	NB_FlowIntervalInMinutes := os.Getenv("NB_FlowIntervalInMinutes")
+	if NB_FlowIntervalInMinutes == "" {
 		// Default interval is 10 minutes
-		NB_FlowIntervalInMinuets = "10"
-		log.Infof("Env 'NB_FlowIntervalInMinuets' is not set, using default: '%s'", NB_FlowIntervalInMinuets)
+		NB_FlowIntervalInMinutes = "10"
+		log.Infof("Env 'NB_FlowIntervalInMinutes' is not set, using default: '%s'", NB_FlowIntervalInMinutes)
 	} else {
-		log.Debugf("Env 'NB_FlowIntervalInMinuets' was set to: '%s'", NB_FlowIntervalInMinuets)
+		log.Debugf("Env 'NB_FlowIntervalInMinutes' was set to: '%s'", NB_FlowIntervalInMinutes)
 	}
 
-	flowInterval, err := time.ParseDuration(NB_FlowIntervalInMinuets + "m")
+	flowInterval, err := time.ParseDuration(NB_FlowIntervalInMinutes + "m")
 	if err != nil {
-		log.Errorf("Failed to parse 'NB_FlowIntervalInMinuets' value '%s': %v", NB_FlowIntervalInMinuets, err)
+		log.Errorf("Failed to parse 'NB_FlowIntervalInMinutes' value '%s': %v", NB_FlowIntervalInMinutes, err)
 		flowInterval = 10 * time.Minute // Fallback to default if parsing fails
 	}
 
